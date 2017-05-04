21030976f8460f0571180bee57a73715449fc0b3
amies <amies.shen@gmail.com>
2017-05-03 23:24:09 -0700

    complete...

--- routes/tasks.js
+++ routes/tasks.js
@@ -6,10 +6,13 @@ module.exports = app =>{
 	//	});
 	//});
 	app.route("/tasks")
+	    .all(app.auth.authenticate())
 	    .get((req, res) => {
 		// "/tasks": List tasks
 		console.log("=====get==");
-		Tasks.findAll({})
+		Tasks.findAll({
+			    where: {user_id: req.user.id}
+			})
 			.then(result => res.json(result))
 			.catch(error => {
 				res.status(412).json({msg: error.message});
@@ -17,6 +20,7 @@ module.exports = app =>{
 	    })	
 	    .post((req, res) => {
 		// "/tasks": Save new task
+		req.body.user_id = req.user.id;
 		Tasks.create(req.body)
 			.then(result => res.json(result))
 			.catch(error => {
@@ -24,9 +28,13 @@ module.exports = app =>{
 			});
 	    });
 	app.route("/tasks/:id")
+	    .all(app.auth.authenticate())
 	    .get((req, res) => {
 		//"tasks/1": Find a task
-		Tasks.findOne({where: req.params})
+		Tasks.findOne({where: {
+			    id: req.params.id,
+			    user_id: req.user.id
+			}})
 			.then(result => {
 			    if(result) {
 				res.json(result);	
@@ -40,7 +48,10 @@ module.exports = app =>{
 	    })
 	    .put((req, res) => {
 		//"tasks/1": Update a task
-		Tasks.update(req.body, {where: req.params})
+		Tasks.update(req.body, {where: {
+			    id: req.params.id,
+			    user_id: req.user.id
+			}})
 			.then(result => res.sendStatus(204))
 			.catch(error => {
 				res.status(412).json({msg: error.message});
@@ -48,7 +59,10 @@ module.exports = app =>{
 	    })
 	    .delete((req, res) => {
 		//"tasks/1": Delete a task
-		Tasks.destroy({where: req.params})
+		Tasks.destroy({where: {
+			    id: req.params.id,
+			    user_id: req.user.id
+			}})
 			.then(result => res.sendStatus(204))
 			.catch(error => {
 			    res.status(412).json(412).json({msg: error.message});;
