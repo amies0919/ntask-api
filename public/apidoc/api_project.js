8f0c7c0b2f3a395e9b73dd2051416cd733d25993
amies <amies.shen@gmail.com>
2017-05-04 23:29:09 -0700

    add clusters

--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-05-05T03:10:14.189Z",
+    "time": "2017-05-05T06:28:13.008Z",
     "url": "http://apidocjs.com",
     "version": "0.15.1"
   }
