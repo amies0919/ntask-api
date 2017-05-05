e6f44e9f1d26dffb588f17c77f16d4072195d78c
amies <amies.shen@gmail.com>
2017-05-05 00:38:08 -0700

    add https

--- libs/boot.js
+++ libs/boot.js
@@ -1,7 +1,14 @@
+import https from "https";
+import fs from "fs";
 module.exports = app =>{
     if (process.env.NODE_ENV !== "test") {
+	const credentials = {
+	    key: fs.readFileSync("amies.key", "utf8"),
+	    cert: fs.readFileSync("amies.cert", "utf8")
+	}
     	app.db.sequelize.sync().done( () => {
-    	    app.listen(app.get("port"), () => {
+	    https.createServer(credentials, app)
+    	        .listen(app.get("port"), () => {
 		console.log("NTask API - Port" + app.get("port"));
     	    });
     	});
