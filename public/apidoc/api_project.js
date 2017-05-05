052ed2123b4c9327ed33c9acdd4dba7a37a5cb20
amies <amies.shen@gmail.com>
2017-05-04 20:20:19 -0700

    add apidoc

---
+++ public/apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Node Task API - Documentation",
+  "template": {
+    "forceLanguage": "en"
+  },
+  "version": "1.0.0",
+  "description": "Task list API",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-05-05T03:10:14.189Z",
+    "url": "http://apidocjs.com",
+    "version": "0.15.1"
+  }
+});
