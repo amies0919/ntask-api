6be51193989eb70c685d6e2907a581537778e1fc
YangXin <xyang.cq@gmail.com>
2018-08-14 21:06:28 +0800

    add douban api

--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -1,16 +1,1 @@
-define({
-  "name": "Node Task API - Documentation",
-  "template": {
-    "forceLanguage": "en"
-  },
-  "version": "1.0.0",
-  "description": "Task list API",
-  "sampleUrl": false,
-  "apidoc": "0.2.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2017-05-08T03:20:41.946Z",
-    "url": "http://apidocjs.com",
-    "version": "0.15.1"
-  }
-});
+define({  "name": "Node Task API - Documentation",  "template": {    "forceLanguage": "en"  },  "version": "1.0.0",  "description": "Task list API",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2018-08-14T10:12:04.009Z",    "url": "http://apidocjs.com",    "version": "0.15.1"  }});
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,59 @@
+var http = require('http');
 module.exports = app =>{
+	app.get("/douban/*",(req, res) => {
+		var _fn = {
+			getData: function(path, callback){
+				var _this = this;
+				http.get({
+					hostname: 'api.douban.com',
+					path: '/' + path
+				}, function(res){
+					var body = [];
+					res.on('data', function(chunk){
+						body.push(chunk);
+					});
+					res.on('end', function(){
+						body = Buffer.concat(body);
+						var bodyObject = JSON.parse(body.toString());
+						if(bodyObject && bodyObject.subjects){
+							bodyObject.subjects = _this.changeImgUrl(bodyObject.subjects,'images');
+						}
+						if(bodyObject && bodyObject.casts){
+							bodyObject.casts = _this.changeImgUrl(bodyObject.casts,'avatars');
+						}
+						if(bodyObject && bodyObject.directors){
+							bodyObject.directors = _this.changeImgUrl(bodyObject.directors,'avatars');
+						}
+						if(bodyObject && bodyObject.images){
+							bodyObject.images = _this.changeImgUrl(bodyObject.images, 'images');
+						}
+						callback(bodyObject);
+					})
+				})
+			},
+			//change im7 to img3
+			changeImgUrl: function(data,key){
+				var result = data;
+				if(result && result.forEach){
+					result.forEach(element => {
+					for(var ele in element[key]){
+						element[key][ele] = element[key][ele].replace('img7','img3');
+					}	
+					});
+				}else{
+					for(var ele in result){
+						result[ele] = result[ele].replace('img7','img3');
+					}
+				}
+				
+				return result;
+			}
+		};
+		var path = req.originalUrl.replace('/douban/','');
+		_fn.getData(path, function(data){
+			res.send(data);
+		});
+	})
  	/**
 	 * @api {get} /API Status
 	 * @apiGroup Status
@@ -10,4 +65,5 @@ module.exports = app =>{
 	app.get("/", (req, res) => {
 		res.json({ status: "NTask API" });
 	});
+
 };
